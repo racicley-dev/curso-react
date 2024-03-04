08d64c987c048157e25f042f6e6a0c83ce325fe1
Racicley <racicley@gmail.com>
2024-03-04 14:45:25 -0300

    feat: inclusão dos tests dos componentes PostCard e Button

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import { PostCard } from '.' 
+import { postCardPropMock } from './mock';
+
+
+const props = postCardPropMock;
+
+describe('<PostCard />', ()=>{
+    it('should render PostCard correctly', ()=>{
+        const { debug } = render(<PostCard {...props} />);
+        //debug();
+
+        expect(screen.getByRole('img', {name: 'title 1'}))
+        .toHaveAttribute('src', 'img/img.png');
+
+        expect(screen.getByRole('heading', { name: /title/i })).toBeInTheDocument();
+
+        expect(screen.getByText('body 1')).toBeInTheDocument();
+    });
+
+
+    //Teste de snapshot
+    it('should match snapshot', ()=>{
+        const { container } = render(<PostCard {...props} />);
+        expect(container.firstChild).toMatchSnapshot();
+
+
+    });
+});
